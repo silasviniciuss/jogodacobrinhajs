8c65e3ec59b1b28e52227102fce9b0167ffbd03e
silasviniciuss <codando1@gmail.com>
2021-06-27 12:09:12 -0300

    criando os movimento da minhoca

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ snake[0] = {
     x: 8 * box,
     y: 8 * box
 }
+let direction = "right";
 
 function criarBG(){
     context.fillStyle = "lightgreen";
@@ -19,5 +20,34 @@ function criarCobrinha(){
     }
 }
 
-criarBG();
-criarCobrinha();
+function iniciarJogo(){
+    criarBG();
+    criarCobrinha();
+
+    //ponto de partida da cobrinha
+    let snakeX = snake[0].x;
+    let snakeY = snake[0].y;
+
+    //coordenadas para o lado que a cobra vai
+    if(direction == 'right') snakeX +=box; 
+    if(direction == 'left') snakeX -=box; 
+
+    if(direction == 'Up') snakeY -=box; 
+    if(direction == 'Donw') snakeY +=box; 
+
+    // retirando o ultimo elemento da array
+    snake.pop();
+
+    //acrescetando um elemento a frente(cabeça)
+    let newHead={
+        x: snakeX,
+        y: snakeY
+    }
+    snake.unshift(newHead);
+}
+
+//tempo para inicialização do jogo
+let jogo = setInterval(iniciarJogo,100);
+
+
+
